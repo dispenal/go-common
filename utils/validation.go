2199c621cf195c4701f16f5a8b84fd8dab3daaf1
Kholiq <kholiqcode@gmail.com>
2023-11-17 14:58:45 +0700

    feat: create validation utility

---
+++ utils/validation.go
@@ -0,0 +1,57 @@
+package common_utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type ValidationError struct {
+	Message string
+}
+
+func (ve *ValidationError) Error() string {
+	return fmt.Sprintf("validation error: message %s", ve.Message)
+}
+
+type ValidationErrors struct {
+	Errors     []ValidationError
+	StatusCode int
+}
+
+func (ve *ValidationErrors) Error() string {
+	return fmt.Sprintf("validation errors: status code %d, message %s", ve.StatusCode, ve.Errors[0].Message)
+}
+
+func PanicValidationError(errors []ValidationError, statusCode int) {
+	validationErrors := ValidationErrors{
+		Errors:     errors,
+		StatusCode: statusCode,
+	}
+	panic(validationErrors)
+}
+
+func ValidateStruct(data interface{}) {
+	var validationErrors []ValidationError
+	validate := validator.New()
+	errorValidate := validate.Struct(data)
+
+	if errorValidate != nil {
+		for _, err := range errorValidate.(validator.ValidationErrors) {
+			var validationError ValidationError
+			validationError.Message = strings.Split(err.Error(), "Error:")[1]
+			validationErrors = append(validationErrors, validationError)
+		}
+		PanicValidationError(validationErrors, 400)
+	}
+}
+
+func ValidateBodyPayload(body io.ReadCloser, output interface{}) {
+	err := json.NewDecoder(body).Decode(output)
+	PanicIfAppError(err, "failed when decode body payload", 400)
+
+	ValidateStruct(output)
+}
---
+++ utils/validation_test.go
@@ -0,0 +1,65 @@
+package common_utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPanicValidationError(t *testing.T) {
+	defer func() {
+		err := recover()
+
+		validationErrors, isValidationErrors := err.(ValidationErrors)
+		assert.Equal(t, true, isValidationErrors)
+		assert.Equal(t, validationErrors.StatusCode, 400)
+		assert.Equal(t, "validation errors", validationErrors.Errors[0].Message)
+	}()
+
+	validationError := []ValidationError{{Message: "validation errors"}}
+	PanicValidationError(validationError, 400)
+}
+
+func TestValidateStruct(t *testing.T) {
+	defer func() {
+		err := recover()
+
+		validationErrors, isValidationErrors := err.(ValidationErrors)
+		assert.Equal(t, true, isValidationErrors)
+		assert.Equal(t, 400, validationErrors.StatusCode)
+		assert.Equal(t, "Field validation for 'Email' failed on the 'email' tag", validationErrors.Errors[0].Message)
+
+	}()
+	type testInput struct {
+		Email    string `json:"email" validate:"required,email"`
+		Username string `json:"username" validate:"min=3,max=10"`
+		Password string `json:"password" validate:"min=8,max=15"`
+	}
+
+	input := testInput{
+		Email:    "test@test",
+		Username: "Testing",
+		Password: "xxxxxxxx",
+	}
+
+	ValidateStruct(&input)
+}
+
+func TestValidateBodyPayload(t *testing.T) {
+	type testInput struct {
+		Success bool `json:"success"`
+	}
+	input := testInput{
+		Success: true,
+	}
+	body, err := json.Marshal(input)
+	assert.NoError(t, err)
+	reader := bytes.NewReader(body)
+	var output testInput
+
+	ValidateBodyPayload(io.NopCloser(reader), &output)
+	assert.Equal(t, true, output.Success)
+}
