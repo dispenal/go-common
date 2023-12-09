c2c715d155c8827cf409bd9f0b91615c710b5533
Kholiq <kholiqcode@gmail.com>
2023-12-09 20:32:02 +0700

    feat: add password utils

---
+++ utils/password.go
@@ -0,0 +1,12 @@
+package common_utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashedPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
+
+func ComparedPasswrd(hashedPassword string, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
---
+++ utils/password_test.go
@@ -0,0 +1,23 @@
+package common_utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHashPassword(t *testing.T) {
+	password := "password"
+	hashedPassword, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+}
+
+func TestComparePassword(t *testing.T) {
+	password := "password"
+	hashedPassword, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+	err = ComparedPasswrd(hashedPassword, password)
+	require.NoError(t, err)
+}
