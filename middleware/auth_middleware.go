261e4effca0d9e67c66edad2293809f6a0b64b39
Kholiq <kholiqcode@gmail.com>
2023-12-10 21:47:08 +0700

    feat: add middleware check refresh token

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -36,7 +36,35 @@ func (m *AuthMiddlewareImpl) CheckIsAuthenticated(next http.Handler) http.Handle
 			common_utils.PanicIfError(common_utils.CustomErrorWithTrace(err, "invalid token", 401))
 		}
 
-		if payload.TokenType == "" {
+		if payload.TokenType != "access" {
+			common_utils.PanicIfError(common_utils.CustomError("invalid token type", 401))
+		}
+
+		if payload.Status != "active" {
+			common_utils.PanicIfError(common_utils.CustomError("inactive user can't access this route", 403))
+		}
+
+		ctx := r.WithContext(jwtMaker.AppendRequestCtx(r, jwtMaker.JWT_PAYLOAD, payload))
+
+		next.ServeHTTP(w, ctx)
+	})
+}
+
+func (m *AuthMiddlewareImpl) CheckIsRefresh(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := r.Header.Get("Authorization")
+
+		if header == "" || !strings.Contains(header, "Bearer ") {
+			common_utils.PanicIfError(common_utils.CustomError("unauthorized", 401))
+		}
+
+		token := strings.Split(header, " ")[1]
+		payload, err := m.jwt.VerifyToken(token)
+		if err != nil {
+			common_utils.PanicIfError(common_utils.CustomErrorWithTrace(err, "invalid token", 401))
+		}
+
+		if payload.TokenType != "refresh" {
 			common_utils.PanicIfError(common_utils.CustomError("invalid token type", 401))
 		}
 
