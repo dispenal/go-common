b05dd27d39961f374e53a754656213847be500f4
Kholiq <kholiqcode@gmail.com>
2023-12-13 15:44:54 +0700

    feat: add metic utils

--- tracer/utils.go
+++ tracer/utils.go
@@ -3,9 +3,11 @@ package tracer
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -51,4 +53,38 @@ func TraceWithErr(span trace.Span, err error) error {
 	}
 
 	return err
+}
+
+func MetricLatency(ctx context.Context, span trace.Span, meter metric.Meter, attributes []attribute.KeyValue) {
+	startTime := time.Now()
+	latencyMs := float64(time.Since(startTime)) / 1e6
+
+	requestLatency, _ := meter.Float64Histogram(
+		"request_latency",
+		metric.WithDescription("The latency of requests processed"),
+	)
+
+	requestLatency.Record(ctx, latencyMs, metric.WithAttributes(attributes...))
+
+	span.SetAttributes(attribute.Float64("latency", latencyMs))
+}
+
+func MetricCount(ctx context.Context, meter metric.Meter, attributes []attribute.KeyValue) {
+	requestCount, _ := meter.Int64Counter(
+		"request_counts",
+		metric.WithDescription("The number of requests processed"),
+	)
+
+	requestCount.Add(ctx, 1, metric.WithAttributes(attributes...))
+
+}
+
+func MetricLineCount(ctx context.Context, meter metric.Meter, attributes []attribute.KeyValue) {
+	lineCounts, _ := meter.Int64Counter(
+		"line_counts",
+		metric.WithDescription("The counts of the lines in"),
+	)
+
+	lineCounts.Add(ctx, 1, metric.WithAttributes(attributes...))
+
 }
