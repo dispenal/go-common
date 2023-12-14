613b80536c76318dc6c2799c965ebdea827490d7
Kholiq <kholiqcode@gmail.com>
2023-12-14 15:23:54 +0700

    fix: wrap recovery middleware with otelhttp

--- middleware/recovery_middleware.go
+++ middleware/recovery_middleware.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	common_utils "github.com/dispenal/go-common/utils"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -56,7 +57,7 @@ func Recovery(next http.Handler) http.Handler {
 }
 
 func RecoveryTracer(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return otelhttp.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			err := recover()
 			if err != nil {
@@ -110,5 +111,5 @@ func RecoveryTracer(next http.Handler) http.Handler {
 			}
 		}()
 		next.ServeHTTP(w, r)
-	})
+	}), "panic-recovery")
 }
