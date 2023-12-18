a3abe355427138215ec73e2c4f29f7e34f18154b
Kholiq <kholiqcode@gmail.com>
2023-12-19 02:22:13 +0700

    feat: add header carrier

--- middleware/tracer_middleware.go
+++ middleware/tracer_middleware.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 )
 
 func TraceHttp(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		operation := fmt.Sprintf("%s %s", r.Method, r.URL.Path)
 
+		ctx := r.Context()
+
+		otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(r.Header))
+
+		r = r.WithContext(ctx)
 		otelhttp.NewHandler(next, operation).ServeHTTP(rw, r)
 	})
 }
