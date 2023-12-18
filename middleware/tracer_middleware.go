4aece37277df6cdc9812b4d458640e8dff6b8f20
Kholiq <kholiqcode@gmail.com>
2023-12-19 02:37:51 +0700

    fix: trace http middleware

--- middleware/tracer_middleware.go
+++ middleware/tracer_middleware.go
@@ -6,16 +6,23 @@ import (
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func TraceHttp(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		operation := fmt.Sprintf("%s %s", r.Method, r.URL.Path)
 
-		option := []otelhttp.Option{
-			otelhttp.WithPropagators(otel.GetTextMapPropagator()),
-		}
+		ctx := r.Context()
 
-		otelhttp.NewHandler(next, operation, option...).ServeHTTP(rw, r)
+		span := trace.SpanFromContext(ctx)
+		defer span.End()
+
+		otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(r.Header))
+
+		req := r.WithContext(trace.ContextWithSpan(ctx, span))
+
+		otelhttp.NewHandler(next, operation).ServeHTTP(rw, req)
 	})
 }
