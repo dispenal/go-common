dd5e26c6685cb8437f481c50804ebad1547cf2c8
Kholiq <kholiqcode@gmail.com>
2023-12-22 18:30:44 +0700

    feat: add search field and value

--- utils/pagination.go
+++ utils/pagination.go
@@ -6,12 +6,14 @@ import (
 )
 
 type Pagination[T any] struct {
-	Limit      int    `json:"limit,omitempty"`
-	Page       int    `json:"page,omitempty"`
-	Sort       string `json:"sort,omitempty"`
-	TotalRows  int    `json:"totalRows,omitempty"`
-	TotalPages int    `json:"totalPages,omitempty"`
-	Rows       []T    `json:"rows,omitempty"`
+	SearchField string `json:"searchField,omitempty"`
+	SearchValue string `json:"searchValue,omitempty"`
+	Limit       int    `json:"limit,omitempty"`
+	Page        int    `json:"page,omitempty"`
+	Sort        string `json:"sort,omitempty"`
+	TotalRows   int    `json:"totalRows,omitempty"`
+	TotalPages  int    `json:"totalPages,omitempty"`
+	Rows        []T    `json:"rows,omitempty"`
 }
 
 func ValidatePagination[T any](r *http.Request) *Pagination[T] {
@@ -29,10 +31,14 @@ func ValidatePagination[T any](r *http.Request) *Pagination[T] {
 	if sortBy == "" {
 		sortBy = "createdAt"
 	}
+	searchField := r.URL.Query().Get("searchField")
+	searchValue := r.URL.Query().Get("searchValue")
 	return &Pagination[T]{
-		Page:  page,
-		Limit: pageSize,
-		Sort:  sortBy,
+		SearchField: searchField,
+		SearchValue: searchValue,
+		Page:        page,
+		Limit:       pageSize,
+		Sort:        sortBy,
 	}
 }
 
