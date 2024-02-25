6241f4dad1744cd7107f19f6bef5d39c098b2726
Kholiq <kholiqcode@gmail.com>
2024-02-26 01:39:56 +0700

    fix: remove log error

--- utils/image.go
+++ utils/image.go
@@ -2,7 +2,6 @@ package common_utils
 
 import (
 	"encoding/base64"
-	"fmt"
 	"io"
 	"net/http"
 )
@@ -10,14 +9,12 @@ import (
 func ImageUriToBase64(uri string) (string, error) {
 	resp, err := http.Get(uri)
 	if err != nil {
-		LogError(fmt.Sprintf("Error fetching image: %s", err))
 		return "", err
 	}
 	defer resp.Body.Close()
 
 	imageData, err := io.ReadAll(resp.Body)
 	if err != nil {
-		LogError(fmt.Sprintf("Error reading image data: %s", err))
 		return "", err
 	}
 
